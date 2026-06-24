7b08b4bf0222ad3243aced289d3eccdb89815865
kauansilva1465285@gmail.com <kauansilva1465285@gmail.com>
2026-06-24 08:30:47 -0300

    "Criar página inicial da Monster Energy"

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+
+  return (
+    <section>
+
+        <h1>Monster Energy</h1>
+        <p>Unleash The Beast</p>
+
+      {/* Botão principal */}
+      <button>Ver Produtos</button>
+    </section>
+  )
+}
